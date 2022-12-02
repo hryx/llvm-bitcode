9d5bfd539d7bdaff332c77d5e42dcc38be9af563
Stevie Hryciw <codroid@gmail.com>
2022-12-01 16:19:02 -0800

    Parse values using abbrevs; read module source filename

--- src/llvm_bitcode.zig
+++ src/llvm_bitcode.zig
@@ -27,7 +27,10 @@ const BlockInfo = struct {
     record_names: std.AutoHashMapUnmanaged(u32, []const u8) = .{},
 };
 
-const Abbrev = u8; // TODO
+const Abbrev = struct {
+    id_encoding: bitstream.AbbrevDefOp,
+    op_encoding: []bitstream.AbbrevDefOp,
+};
 
 pub fn Parser(comptime ReaderType: type) type {
     return struct {
@@ -78,7 +81,7 @@ pub fn Parser(comptime ReaderType: type) type {
             var bc: Bitcode = undefined;
 
             blocks: while (true) {
-                const abbrev_id = self.bitstream_reader.readAbbreviationId(2) catch |err| switch (err) {
+                const abbrev_id = self.readAbbrevId() catch |err| switch (err) {
                     error.EndOfStream => {
                         if (self.bitstream_reader.pos % 32 != 0) {
                             return err;
@@ -145,7 +148,7 @@ pub fn Parser(comptime ReaderType: type) type {
         fn parseBlockInfo(self: *Self) !?ParseError {
             var block_id: ?Bitcode.BlockId = null;
             while (true) {
-                const id = try self.bitstream_reader.readAbbreviationId(self.abbrev_id_width);
+                const id = try self.readAbbrevId();
                 std.log.info("BLOCKINFO record code {}", .{id});
                 switch (id) {
                     .END_BLOCK => {
@@ -187,7 +190,7 @@ pub fn Parser(comptime ReaderType: type) type {
             }
             self.found_module = true;
             while (true) {
-                const id = try self.bitstream_reader.readAbbreviationId(self.abbrev_id_width);
+                const id = try self.readAbbrevId();
                 switch (id) {
                     .END_BLOCK => {
                         try self.bitstream_reader.endBlock();
@@ -248,7 +251,33 @@ pub fn Parser(comptime ReaderType: type) type {
                         const abbr = self.getAbbrev(.MODULE_BLOCK_ID, abbr_id) orelse {
                             return try self.parseError("abbrev {} not yet defined for module block", .{abbr_id});
                         };
-                        std.log.info("TODO: found abbr {}: {c}", .{ abbr_id, abbr });
+                        const code_id = @intToEnum(Bitcode.Module.Code, try self.parseAbbrevRecordCode(abbr));
+                        std.log.info("  module block: got abbreviated code for {}", .{code_id});
+                        switch (code_id) {
+                            .MODULE_CODE_SOURCE_FILENAME => {
+                                const read = try self.parseAbbrevVal(abbr.op_encoding, &bc.module.source_filename);
+                                assert(read == 2);
+                                std.log.info("got source filename: {s}", .{bc.module.source_filename});
+                            },
+                            .MODULE_CODE_VERSION,
+                            .MODULE_CODE_TRIPLE,
+                            .MODULE_CODE_DATALAYOUT,
+                            .MODULE_CODE_ASM,
+                            .MODULE_CODE_SECTIONNAME,
+                            .MODULE_CODE_DEPLIB,
+                            .MODULE_CODE_GLOBALVAR,
+                            .MODULE_CODE_FUNCTION,
+                            .MODULE_CODE_ALIAS,
+                            .MODULE_CODE_GCNAME,
+                            => {
+                                std.debug.panic("TODO: parse abbreviated module code {}", .{code_id});
+                            },
+                            _ => {
+                                return try self.parseError("unknown module code {} in abbrev record", .{@enumToInt(code_id)});
+                            },
+                        }
+                        // try self.parseWithAbbrev(abbr);
+                        // std.log.info("TODO: found abbr {}: {any}", .{ abbr_id, abbr });
                     },
                 }
             }
@@ -261,30 +290,69 @@ pub fn Parser(comptime ReaderType: type) type {
                 return try self.parseError("duplicate IDENTIFICATION block", .{});
             }
             self.found_identification = true;
-            const id = try self.bitstream_reader.readAbbreviationId(self.abbrev_id_width);
+            const id = try self.readAbbrevId();
             _ = id;
             _ = bc;
             // TODO: bc.identification = x;
             return null;
         }
 
+        fn readAbbrevId(self: *Self) !bitstream.AbbreviationId {
+            return self.bitstream_reader.readAbbreviationId(self.abbrev_id_width);
+        }
+
+        /// Parse bits during DEFINE_ABBREV, storing the abbreviation in
+        /// the given block. An abbrev's ID is based on its index when appended.
         fn defineAbbrev(self: *Self, block_id: Bitcode.BlockId) !?ParseError {
             const index = @enumToInt(block_id);
             assert(index < block_info_len);
-            const block = &self.block_info[index];
+            const abbrevs = &self.block_info[index].abbrevs;
 
-            const arg_count = try self.bitstream_reader.readVbr(u32, 5);
-            std.log.info("DEFINE_ABBREV block {} num ops {}", .{ block_id, arg_count });
-            var i: u32 = 0;
-            while (i < arg_count) : (i += 1) {
-                const op = try self.bitstream_reader.readAbbrevDefOp();
-                std.log.info("  ABBREV OP: {any}", .{op});
+            const op_count = try self.bitstream_reader.readVbr(u32, 5);
+            if (op_count == 0) {
+                return try self.parseError("empty op list in abbrev", .{});
             }
 
-            try block.abbrevs.append(self.arena.allocator(), 'A'); // TODO
+            const id_encoding = try self.bitstream_reader.readAbbrevDefOp();
+            switch (id_encoding) {
+                .literal => {},
+                .encoded => |enc| switch (enc) {
+                    .fixed, .vbr, .char6 => {},
+                    .array => return try self.parseError("abbrev record code cannot be encoded as array", .{}),
+                    .blob => return try self.parseError("abbrev record code cannot be encoded as blob", .{}),
+                },
+            }
+            var abbrev = Abbrev{
+                .id_encoding = id_encoding,
+                .op_encoding = &.{},
+            };
+            std.log.info("DEFINE_ABBREV block {} num ops {}", .{ block_id, op_count });
+
+            const arg_count = op_count - 1;
+            if (arg_count > 0) {
+                const ops = try self.arena.allocator().alloc(bitstream.AbbrevDefOp, arg_count);
+
+                var i: u32 = 0;
+                var last_op_was_array = false;
+                while (i < arg_count) : (i += 1) {
+                    const op = try self.bitstream_reader.readAbbrevDefOp();
+                    std.log.info("  ABBREV OP: {any}", .{op});
+                    last_op_was_array = op == .encoded and op.encoded == .array;
+                    ops[i] = op;
+                }
+                if (last_op_was_array) {
+                    return try self.parseError("abbrev def array op was not followed by type op", .{});
+                }
+
+                abbrev.op_encoding = ops;
+            }
+
+            try abbrevs.append(self.arena.allocator(), abbrev);
             return null;
         }
 
+        /// Returns an abbrev encoding previously defined with DEFINE_ABBREV
+        /// for the given block, or null if the abbrev ID has not been defined.
         fn getAbbrev(self: *Self, block_id: Bitcode.BlockId, abbr_id: u32) ?Abbrev {
             const first_id = bitstream.AbbreviationId.first_application_abbrev_id;
             assert(abbr_id >= first_id);
@@ -295,6 +363,95 @@ pub fn Parser(comptime ReaderType: type) type {
                 return null;
             }
             return abbrs[adjusted_id];
+        }
+
+        /// When referencing an abbreviated record, call this to get the record code.
+        /// For non-literal code IDs, this reads more bits.
+        fn parseAbbrevRecordCode(self: *Self, abbr: Abbrev) !u32 {
+            return switch (abbr.id_encoding) {
+                .literal => |lit| @intCast(u32, lit), // TODO cast is temp hack
+                .encoded => |enc| switch (enc) {
+                    .fixed => |width| try self.bitstream_reader.readInt(u32, width),
+                    .vbr => |width| try self.bitstream_reader.readVbr(u32, width),
+                    .char6 => try self.bitstream_reader.readChar6(),
+                    .array, .blob => unreachable, // rejected during DEFINE_ABBREV
+                },
+            };
+        }
+
+        /// Parse a single value into `dst` using the encoding rules stored in `abbrev_ops`.
+        /// `dst` must be a pointer type and be compatible with the types described by
+        /// the encoding rule in `abbrev_ops`.
+        /// Data encoded as an array expects `dst` to be a `*[]T`.
+        /// This function allocates the slice.
+        ///
+        /// Returns the number of ops consumed, which will be more than one for array ops.
+        ///
+        /// The `abbrev_ops` slice should start at the current op position,
+        /// so subsequent calls should pass a subslice whose start is the total number
+        /// of ops read so far, i.e `ops[read_so_far..]`.
+        fn parseAbbrevVal(self: *Self, abbrev_ops: []bitstream.AbbrevDefOp, dst: anytype) !u16 {
+            assert(abbrev_ops.len > 0);
+
+            const Ptr = @TypeOf(dst);
+            const ptr_info = @typeInfo(Ptr);
+            if (ptr_info != .Pointer or ptr_info.Pointer.size != .One) {
+                @compileError("dst must be a *T");
+            }
+            const Dst = ptr_info.Pointer.child;
+            const dst_info = @typeInfo(Dst);
+
+            switch (dst_info) {
+                .Int => {
+                    switch (abbrev_ops[0]) {
+                        .literal => |lit| {
+                            dst.* = @intCast(Dst, lit);
+                        },
+                        .encoded => |enc| switch (enc) {
+                            .fixed => |width| {
+                                dst.* = try self.bitstream_reader.readInt(Dst, width);
+                            },
+                            .vbr => |width| {
+                                dst.* = try self.bitstream_reader.readVbr(Dst, width);
+                            },
+                            .char6 => {
+                                dst.* = @intCast(Dst, try self.bitstream_reader.readChar6());
+                            },
+                            inline else => |e| {
+                                return try self.parseError("expected int type for abbrev op, got {s}", .{@tagName(e)});
+                            },
+                        },
+                    }
+                    return 1;
+                },
+                .Pointer => |p| {
+                    if (p.size != .Slice) {
+                        @compileError("can only parse abbrev vals into int or slice");
+                    }
+                    const Elem = p.child;
+                    if (@typeInfo(Elem) != .Int) {
+                        @compileError("slice element only supports ints for now");
+                    }
+
+                    var elem_type = abbrev_ops[1];
+                    if (!(elem_type == .encoded and elem_type.encoded == .char6)) {
+                        std.debug.panic("TODO: parse arrays other than []char6, got {}", .{elem_type});
+                    }
+
+                    const len = try self.bitstream_reader.readVbr(usize, 6);
+                    if (len == 0) return 2;
+
+                    const slice = try self.arena.allocator().alloc(Elem, len);
+                    var i: usize = 0;
+                    while (i < len) : (i += 1) {
+                        slice[i] = try self.bitstream_reader.readChar6();
+                    }
+                    dst.* = slice;
+
+                    return 2;
+                },
+                else => @compileError("can only parse abbrev vals into int or slice"),
+            }
         }
 
         fn parseUnabbrevOps(self: *Self, comptime T: type, count: u32) ![]T {
